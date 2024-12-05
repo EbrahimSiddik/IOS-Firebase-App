97a93eb7ca27799080d93dfbe09b3b24bfdd718e
Ebrahim Siddik Zain <ebrahimsiddik865@gmail.com>
2024-12-05 22:34:54 +0600

    firebase authentication

---
+++ swiftui-firebase/swiftui-firebase/swiftui_firebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_firebaseApp.swift
+//  swiftui-firebase
+//
+//  Created by Tanvir Ahmed on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_firebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
